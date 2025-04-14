29b704875947b882c80cf5eeabfbcef8b1ef855c
dakariwrenn <dakari1.wrenn@famu.edu>
2025-04-14 18:24:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 function toggleDetails(element) {
   const details = element.querySelector(".details");
-  if (details.style.display === "block") {
-    details.style.display = "none";
-  } else {
-    details.style.display = "block";
-  }
+  details.style.display = details.style.display === "block" ? "none" : "block";
 }
